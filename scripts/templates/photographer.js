3a54c1ef3deb0ae0d36eeffcc404c863d7b4eb2e
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-02-28 15:45:28 +0100

    🔨 Index page > Photographer js > Add aria-label

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -7,11 +7,13 @@ function photographerTemplate(data) {
         const article = document.createElement('article');
 
         const img = document.createElement('img');
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
         img.setAttribute("alt", name);
+        img.setAttribute("aria-label", portrait);
 
         const h2 = document.createElement('h2');
         h2.innerText = name;
+        h2.setAttribute("aria-label", name);
 
         const locationWrapper = document.createElement('div'); // Créer une div parent
         locationWrapper.classList.add('location');
@@ -19,10 +21,12 @@ function photographerTemplate(data) {
         const City = document.createElement('p');
         City.innerText = `${city},`;
         City.classList.add('from')
+        City.setAttribute("aria-label", city);
 
         const Country = document.createElement('p');
         Country.innerText = `${country}`;
         Country.classList.add('from')
+        Country.setAttribute("aria-label", country);
 
         locationWrapper.appendChild(City); // Ajouter la ville à la div parent
         locationWrapper.appendChild(Country);
@@ -30,10 +34,12 @@ function photographerTemplate(data) {
         const Tagline = document.createElement('p');
         Tagline.innerText = `${tagline}`;
         Tagline.classList.add('tagline')
+        Tagline.setAttribute("aria-label", tagline)
 
         const Price = document.createElement('p');
         Price.innerText = `${price}€/jour`;
         Price.classList.add('price')
+        Price.setAttribute("aria-label", price)
 
         article.appendChild(img);
         article.appendChild(h2);
